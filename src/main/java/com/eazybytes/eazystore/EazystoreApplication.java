9e6bcc3c375a5da46b05244d7ffbfecdfba0df54
rudravaram85 <rajesh.rudravaram85@gmail.com>
2026-07-06 12:37:18 +0530

    initial commit

---
+++ src/main/java/com/eazybytes/eazystore/EazystoreApplication.java
@@ -0,0 +1,15 @@
+package com.eazybytes.eazystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+public class EazystoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazystoreApplication.class, args);
+	}
+
+}
